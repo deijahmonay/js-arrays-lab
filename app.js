3fb7881a522d0e6ae7aa3abcf21ce1e83c785688
deijahmonay <deijah@shack15.com>
2024-07-29 18:04:26 -0400

    Starting number 5

--- app.js
+++ app.js
@@ -52,6 +52,11 @@ let favFood = ['pizza']
 
 console.log('Exercise 4 result:', favFood);
 
+splice(start)
+splice(start, deleteCount)
+splice(start, deleteCount, item0)
+splice(start, deleteCount, item0, item1)
+splice(start, deleteCount, item0, item1, /* …, */ itemN)
 
 
 
