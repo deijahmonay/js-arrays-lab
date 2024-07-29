71487543edc8e284782f9467ed23a11d2ea501f2
deijahmonay <deijah@shack15.com>
2024-07-29 17:55:14 -0400

    Completedd exercise 2 try 2

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ Exercise 1: Define an empty array
 Exercise 1 has been completed for you:
 */
 
-const foods = [];  
+// const foods = [];  
 
-console.log('Exercise 1 result:', foods);
+// console.log('Exercise 1 result:', foods);
 
+/*
+Exercise 2: Add strings to the array
 
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+const foods = ['pizza', 'cheeseburger']
+console.log('Exercise 2 result:', foods);
+
+//* I had to comment out the first exercise in order to solve exercise 2
 
 
 
